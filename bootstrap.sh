5917d97f49d76fd7e46cb29d64fcd14545f3d5f0
Savenkov Igor <savenkovigor.dev@gmail.com>
2026-05-20 03:43:53 +0400

    refactor: improve depot_tools cloning logic

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+# If pwd / depot_tools does not exist, clone it
+if [ ! -d "$(pwd)/depot_tools" ]; then
+    echo "Cloning depot_tools into $(pwd)/depot_tools"
+    git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+else
+    echo "depot_tools already exists in $(pwd)/depot_tools"
+fi
+
